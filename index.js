93246f44b02d8d6ec89695475fa38f312024c3fc
Pronepoet <ochiengphillip4@gmail.com>
2022-08-24 15:12:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+addEventListener ('DOMContentLoaded', (event)=>{
+    let par = document.querySelector('p')
+    par.textContent = "This is really cool!"
+});
+onDOMContentLoaded = (event)=>{};
